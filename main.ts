00d2dde0c7143220345cc0ee95d80dcbe3878589
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-06-04 13:12:24 -0400

    changed code

--- main.ts
+++ main.ts
@@ -2,7 +2,27 @@
  *
  * Created by: Kyle and DJ
  * Created on: Jun 2026
- * This program ...
+ * This program will move the conveyor belt for exactly 4 seconds when Button A is pressed.
 */
 
-basic.showString('Hello, World!')
+// Setup
+basic.showIcon(IconNames.Happy)
+robotbit.MotorStopAll()
+
+// button a
+input.onButtonPressed(Button.A, function () {
+    // Show that it is running
+    basic.showIcon(IconNames.Surprised)
+
+    // Starts the conveyor belt to move forward
+    robotbit.StpCarMove(40, 48)
+
+    // Wait for 4 second while it is moves
+    basic.pause(4000)
+
+    // Stops the conveyor belt completeler
+    robotbit.MotorStopAll()
+
+    // Reset icon
+    basic.showIcon(IconNames.Happy)
+})
